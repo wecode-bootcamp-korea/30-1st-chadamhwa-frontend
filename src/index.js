60dd44b01f061ad47521db5748b69716bf514f82
Bueong <pcj0308@gmail.com>
2022-02-28 17:57:13 +0900

    Fix: 초기설정 수정

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Router from './Router';
 import './styles/reset.scss';
 import './styles/common.scss';
 
-ReactDOM.render(document.getElementById('root'));
+ReactDOM.render(<Router />, document.getElementById('root'));
