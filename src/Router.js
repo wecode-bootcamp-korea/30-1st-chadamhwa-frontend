095dfbde842f1326c2c20eb183d5c80b18e79a28
Bueong <pcj0308@gmail.com>
2022-03-03 17:25:01 +0900

    Fix: path 오타 수정

--- src/Router.js
+++ src/Router.js
@@ -11,7 +11,7 @@ function Router() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/Products" element={<Products />} />
+        <Route path="/products" element={<Products />} />
         <Route path="/login" element={<Login />} />
         <Route path="/detail" element={<Detail />} />
         <Route path="/cart" element={<Cart />} />
