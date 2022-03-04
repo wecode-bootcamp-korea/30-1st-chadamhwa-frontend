921824086d3f317005086340da6ad8de968509c7
YunJi Shin <yunji@YunJiui-MacBookPro.local>
2022-03-04 13:56:25 +0900

    Add: Detail 페이지 진행 상황(html, scss)

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -4,50 +4,112 @@ import './Detail.scss';
 function Detail() {
   return (
     <div className="Detail">
-      <section className="rightSide">
-        <h1>수량</h1>
-        <input type="number" placeholder="1" />
-        <p>총 상품 가격</p>
-        <p>0원</p>
-        <p>+(전국택배) 3,000원</p>
-        <p>+(제주도 및 도서산간) 3,000원</p>
-        <p>본 농장에서 50,000원 이상 구매 시 배송비 무료!</p>
-        <button>장바구니 담기</button>
-        <button>바로 구매하기</button>
-      </section>
-      <section className="product">
-        <section className="productIntro">
-          <img className="productImg" src="#" alt="Product image" />
-          <section className="productIntroduction">
-            <h1>하동 녹차</h1>
-            <p>차 한 잔으로 편안함과 차분함을 얻을 수 있어요</p>
-            <p>#진한 녹차 향 #은은한 단 맛 </p>
-            <img src="#" alt="Product review stars" />
-            <a href="#review">[123 리뷰]</a>
-            <p>주종: 약주</p>
-            <p>카페인: 유</p>
-            <p>용량: 500ml</p>
-            <p>배송기간: 2일 이내 배송</p>
-            <p>판매가격:</p>
-            <p>24,000원</p>
-            <p>유통기한: 병입일로부터 90일</p>
-            <p>보관방법: 냉장 보관</p>
+      <div className="intro">
+        <section className="product">
+          <section className="productIntro">
+            <div className="image">
+              <img
+                className="productImg"
+                src="/images/nokcha.jpg"
+                alt="Product"
+              />
+            </div>
+            <div className="productIntroduction">
+              <ul>
+                <li className="name">
+                  <b>하동 녹차</b>
+                </li>
+                <li className="catchphrase">
+                  <span>차 한 잔으로 편안함과 차분함을 얻을 수 있어요!</span>
+                </li>
+                <li className="hashtag">
+                  <span>#진한 녹차 향 #은은한 단 맛 </span>
+                </li>
+                <li className="review">
+                  <img src="#" alt="Product review stars" />
+                  <a href="#review">[123 리뷰]</a>
+                </li>
+                <li className="category">
+                  <span>주종: 약주</span>
+                </li>
+                <li className="isCaffeine">
+                  <span>카페인: 유</span>
+                </li>
+                <li className="volume">
+                  <span>용량: 500ml</span>
+                </li>
+                <li className="delivery">
+                  <span>배송기간: 2일 이내 배송</span>
+                </li>
+                <li className="priceCategory">
+                  <span>판매가격:</span>
+                </li>
+                <li className="price">
+                  <span>24,000원</span>
+                </li>
+                <li className="expirationDate">
+                  <span>유통기한: 별도 표기</span>
+                </li>
+                <li className="howToKeep">
+                  <span>보관방법: 냉장 보관</span>
+                </li>
+              </ul>
+            </div>
           </section>
         </section>
-        <div className="productDetails">
-          <img src="#" alt="Product details" />
+        <div className="rightSide">
+          <ul>
+            <li className="quantity">
+              <span>수량</span>
+            </li>
+            <li>
+              <input type="number" placeholder="1" />
+            </li>
+            <li className="priceIs">
+              <span>총 상품 가격</span>
+            </li>
+            <li className="totalPrice">
+              <span>0원</span>
+            </li>
+            <li className="deliveryFee">
+              <span>+(전국택배) 3,000원</span>
+            </li>
+            <li className="additionalDeliveryFee">
+              <span>+(제주도 및 도서산간) 3,000원</span>
+            </li>
+            <li className="freeDeliveryFee">
+              <span>본 농장에서 50,000원 이상 구매 시 배송비 무료!</span>
+            </li>
+          </ul>
+          <section className="rightSideBtn">
+            <button>장바구니 담기</button>
+            <button>바로 구매하기</button>
+          </section>
         </div>
-      </section>
+      </div>
+      <div className="productDetails">
+        <img src="#" alt="Product details" />
+      </div>
       <div name="review" className="review">
-        <p>리뷰</p>
-        <section>
-          <span>최신순</span>
-          <span>평점 높은 순</span>
-          <span>평점 낮은 순</span>
+        <div className="review">
+          <span className="reviewBtn">리뷰</span>
+        </div>
+        <section className="reviewSort">
+          <button>최신순</button>
+          <button>평점 높은 순</button>
+          <button>평점 낮은 순</button>
         </section>
-        <section>
-          <p>Nickname</p>
+        <section className="reviewList">
+          <div className="nickname">
+            <span>Nickname</span>
+          </div>
+          <div className="boughtWhat">
+            <span>[500ml]하동 녹차</span>
+            <img src="#" alt="review star image" />
+            <span>2022.02.22</span>
+          </div>
         </section>
+        <button className="moreReview">더 많은 리뷰</button>
       </div>
     </div>
   );
