6313f9ca29f9a64f314f1c4b2fb9e1de04400b44
박별 <byul4882@naver.com>
2022-03-07 00:40:01 +0900

    Add : main slide 추가

--- src/pages/Main/Figure/Figure.js
+++ src/pages/Main/Figure/Figure.js
@@ -7,25 +7,33 @@ function Figure() {
       <input className="btn" type="radio" name="slide" id="slide01" checked />
       <input className="btn" type="radio" name="slide" id="slide02" checked />
       <input className="btn" type="radio" name="slide" id="slide03" checked />
+      <input className="btn" type="radio" name="slide" id="slide04" checked />
       <ul className="slide-list">
         <li className="slide-item">
           <a>
-            <label for="slide03" className="prev" />
-            <img src="/images/banner2.png" />
+            <label for="slide04" className="prev" />
+            <img src="/images/banner1.png" />
             <label for="slide02" className="next" />
           </a>
         </li>
         <li className="slide-item">
           <a>
             <label for="slide01" className="prev" />
-            <img src="/images/banner3.png" />
+            <img src="/images/banner4.png" />
             <label for="slide03" className="next" />
           </a>
         </li>
         <li className="slide-item">
           <a>
             <label for="slide02" className="prev" />
-            <img src="/images/banner1.png" />
+            <img src="/images/banner2.png" />
+            <label for="slide04" className="next" />
+          </a>
+        </li>
+        <li className="slide-item">
+          <a>
+            <label for="slide03" className="prev" />
+            <img src="/images/banner3.png" />
             <label for="slide01" className="next" />
           </a>
         </li>
