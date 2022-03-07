c6e1e7c42aa4b7e53105a88076083cf556e170d0
박별 <byul4882@naver.com>
2022-03-07 21:07:52 +0900

    Add : signup 기능추가

--- src/pages/Login/Signup/Signup.js
+++ src/pages/Login/Signup/Signup.js
@@ -1,16 +1,41 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
 import './Signup.scss';
+import { Navigate, useNavigate } from 'react-router';
 
 function Signup() {
   const [signupEmail, setSignUpEmail] = useState('');
   const [signupPw, setSignUpPw] = useState('');
   const [signupRePw, setSignUpRePw] = useState('');
+  const [signupName, setSignUpName] = useState('');
+  const [signupAddress, setSignUpAddress] = useState('');
+  const navigate = useNavigate();
+
   let SignupValid =
-    signupEmail.includes('@') && signupPw.length > 5 && signupPw === signupRePw;
+    signupEmail.includes('@') &&
+    signupPw.length > 5 &&
+    signupPw === signupRePw &&
+    signupName.length > 1;
 
   function mainCondition(event) {
     event.preventDefault();
+    fetch('http://10.58.0.92:8000/users/signup', {
+      method: 'post',
+      body: JSON.stringify({
+        username: signupName,
+        email: signupEmail,
+        password: signupPw,
+        address: signupAddress,
+      }),
+    }).then(response => response.json());
+    // .then(result => {
+    //   if (result.message === 'SIGNUP_SUCCESS') {
+    //     alert('환영합니다!');
+    //     navigate('/');
+    //   } else {
+    //     alert('입력을 확인해주세요!');
+    //   }
+    // });
   }
   function handleInputEmail(event) {
     setSignUpEmail(event.target.value);
@@ -18,21 +43,26 @@ function Signup() {
   function handleInputPw(event) {
     setSignUpPw(event.target.value);
   }
-
-  function joinClient() {
-    fetch('', {
-      method: 'post',
-      body: JSON.stringify({
-        email: signupEmail,
-        password: signupPw,
-      }),
-    });
+  function handleInputName(event) {
+    setSignUpName(event.target.value);
   }
+  function handleInputAddress(event) {
+    setSignUpAddress(event.target.value);
+  }
+
   return (
     <div className="sign-up">
       <h1 className="title"> 회원 가입 </h1>
       <img className="id-card" src="/images/id-card.png" />
       <form>
+        <div className="wrap">
+          <span className="key">이름</span>
+          <input
+            type="text"
+            onChange={handleInputName}
+            placeholder="이름을 입력해주세요"
+          />
+        </div>
         <div className="wrap">
           <span className="key">이메일</span>
           <input
@@ -57,6 +87,14 @@ function Signup() {
               setSignUpRePw(target.value);
             }}
             placeholder="비밀번호를 한번 더 입력해주세요"
+          />
+        </div>
+        <div className="wrap">
+          <span className="key">주소</span>
+          <input
+            type="text"
+            onChange={handleInputAddress}
+            placeholder="주소를 입력해주세요"
           />
         </div>
         <div className="wrap">
