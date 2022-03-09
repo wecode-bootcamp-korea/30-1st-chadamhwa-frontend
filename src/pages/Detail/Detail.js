47272c3700a6b1a168385b5f897b979cc48edc0b
YunJi Shin <yunji@YunJiui-MacBookPro.local>
2022-03-09 21:32:01 +0900

    Modify: Detail 페이지 백엔드와의 소통 부분 수정

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -10,7 +10,7 @@ function Detail() {
   const [reviewData, setReviewData] = useState([]);
   const [rate, setRate] = useState(0);
   const [comment, setComment] = useState('');
-  const price = productData[0].price;
+  const price = Math.floor(productData[0].price);
 
   useEffect(() => {
     fetch('http://10.58.0.92:8000/reviews/comments/7')
@@ -56,112 +56,114 @@ function Detail() {
   };
 
   return (
-    <>
-      <SubNav />
-      <div className="detail">
-        <div className="leftSide">
-          <section className="productIntro">
-            <div className="productMainImage">
-              <img
-                className="productImg"
-                src={productData.thumb_img}
-                alt="Product"
-              />
-            </div>
-            <div className="productIntroduction">
-              <ul>
-                <Intro productData={productData[0]} />
-              </ul>
-            </div>
-          </section>
-          <div className="productDetails">
-            <img
-              className="productDetailImage"
-              src={productData.detail_img}
-              alt="Product details"
-            />
-          </div>
-          <div name="review" className="review">
-            <div id="reviewInDetail" className="review">
-              <button className="reviewBtn">리뷰</button>
-            </div>
-            <form onSubmit={handleReviewData}>
-              <div>
-                <i className="fa fa-star" aria-hidden="true" />
-                <input
-                  type="number"
-                  min="1"
-                  max="5"
-                  placeholder="별점"
-                  onChange={getRate}
-                  value={rate}
+    productData.length > 0 && (
+      <>
+        <SubNav />
+        <div className="detail">
+          <div className="leftSide">
+            <section className="productIntro">
+              <div className="productMainImage">
+                <img
+                  className="productImg"
+                  src={productData[0].thumb_img}
+                  alt="Product"
                 />
               </div>
-              <input
-                type="textarea"
-                placeholder="리뷰 내용"
-                className="commentInput"
-                onChange={getComment}
-                value={comment}
-              />
-              <button>입력</button>
-            </form>
-            <ReviewList reviewData={reviewData} />
-          </div>
-        </div>
-        <div className="rightSideWrapper">
-          <div className="rightSide">
-            <ul>
-              <li className="quantity">
-                <span>수량</span>
-              </li>
-              <li>
-                <div className="quantityCounter">
-                  <button
-                    className={` ${
-                      quantity > 1
-                        ? 'ableDecreaseQuantity'
-                        : 'unableDecreaseQuantity'
-                    }`}
-                    onClick={decreaseQuantity}
-                  >
-                    −
-                  </button>
-                  <span>{quantity}</span>
-                  <button
-                    className="increaseQuantity"
-                    onClick={increaseQuantity}
-                  >
-                    +
-                  </button>
-                </div>
-              </li>
-              <li className="priceIs">
-                <span>총 상품 가격</span>
-              </li>
-              <div className="totalPrice">
-                <li>
-                  <span className="totalPrice">{totalPrice}원</span>
-                </li>
+              <div className="productIntroduction">
+                <ul>
+                  <Intro productData={productData[0]} />
+                </ul>
               </div>
-              <li className="deliveryFee">
-                <span>+(전국택배) 3,000원</span>
-              </li>
-              <li className="additionalDeliveryFee">
-                <span>+(제주도 및 도서산간) 3,000원</span>
-              </li>
-              <li className="freeDeliveryFee">
-                <span>본 농장에서 50,000원 이상 구매 시 배송비 무료!</span>
-              </li>
-            </ul>
-            <section className="rightSideBtn">
-              <button>장바구니 담기</button>
-              <button>바로 구매하기</button>
             </section>
+            <div className="productDetails">
+              <img
+                className="productDetailImage"
+                src={productData[0].detail_img}
+                alt="Product details"
+              />
+            </div>
+            <div name="review" className="review">
+              <div id="reviewInDetail" className="review">
+                <button className="reviewBtn">리뷰</button>
+              </div>
+              <form onSubmit={handleReviewData}>
+                <div>
+                  <i className="fa fa-star" aria-hidden="true" />
+                  <input
+                    type="number"
+                    min="1"
+                    max="5"
+                    placeholder="별점"
+                    onChange={getRate}
+                    value={rate}
+                  />
+                </div>
+                <input
+                  type="textarea"
+                  placeholder="리뷰 내용"
+                  className="commentInput"
+                  onChange={getComment}
+                  value={comment}
+                />
+                <button>입력</button>
+              </form>
+              <ReviewList reviewData={reviewData} />
+            </div>
+          </div>
+          <div className="rightSideWrapper">
+            <div className="rightSide">
+              <ul>
+                <li className="quantity">
+                  <span>수량</span>
+                </li>
+                <li>
+                  <div className="quantityCounter">
+                    <button
+                      className={` ${
+                        quantity > 1
+                          ? 'ableDecreaseQuantity'
+                          : 'unableDecreaseQuantity'
+                      }`}
+                      onClick={decreaseQuantity}
+                    >
+                      −
+                    </button>
+                    <span>{quantity}</span>
+                    <button
+                      className="increaseQuantity"
+                      onClick={increaseQuantity}
+                    >
+                      +
+                    </button>
+                  </div>
+                </li>
+                <li className="priceIs">
+                  <span>총 상품 가격</span>
+                </li>
+                <div className="totalPrice">
+                  <li>
+                    <span className="totalPrice">{totalPrice}원</span>
+                  </li>
+                </div>
+                <li className="deliveryFee">
+                  <span>+(전국택배) 3,000원</span>
+                </li>
+                <li className="additionalDeliveryFee">
+                  <span>+(제주도 및 도서산간) 3,000원</span>
+                </li>
+                <li className="freeDeliveryFee">
+                  <span>본 농장에서 50,000원 이상 구매 시 배송비 무료!</span>
+                </li>
+              </ul>
+              <section className="rightSideBtn">
+                <button>장바구니 담기</button>
+                <button>바로 구매하기</button>
+              </section>
+            </div>
           </div>
         </div>
-      </div>
-    </>
+      </>
+    )
   );
 }
 
