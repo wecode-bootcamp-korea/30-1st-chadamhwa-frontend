c293e47604244cfe2e59f4f1c23135d852d3d1d0
박별 <byul4882@naver.com>
2022-03-10 16:03:23 +0900

    complete

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Nav.scss';
+
+function Nav() {
+  return (
+    <nav className="nav">
+      <div className="inner">
+        <div className="logo">
+          <img src="images/logo.jpeg" alt="logo" />
+          <h1 className="logo-title">ㅊㄷㅎ</h1>
+        </div>
+        <div className="menu">
+          <div className="market-btn">
+            <a href="/">담화마켓(스토어)</a>
+          </div>
+          <div className="login-btn">
+            <a href="/">로그인</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+export default Nav;
